f690ca5287fa8767952c1dfb2d7c53b1f15a2893
Milind Patel <milindpatel63@gmail.com>
2021-05-30 23:21:10 +0530

    Update main.go

--- main.go
+++ main.go
@@ -49,8 +49,8 @@ func changeURL(w http.ResponseWriter, r *http.Request) {
 
 	if key != secret {
 		w.WriteHeader(http.StatusForbidden)
-		log.Printf("here this ",secret,url)
-		fmt.Fprintf(w, "here this ",secret,url)
+		log.Printf("here this ",secret,URL)
+		fmt.Fprintf(w, "here this ",secret,URL)
 		fmt.Fprintf(w, "You are Forbidden")
 		return
 	}
